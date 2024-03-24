62ef28408e607fb4fd9572b165f80d814a4dac44
Lukas Scheller <lukas2121@icloud.com>
2024-03-24 23:01:00 +0100

    Use naive Djikstra algorithm to trace back the path

--- ginko/src/dts/importer.rs
+++ ginko/src/dts/importer.rs
@@ -16,17 +16,17 @@ impl<V> CyclicDependencyError<V> {
 /// This struct operates on easy cloneable objects (such as strings or ints)
 /// and can provide the dependency map later-on.
 pub struct CyclicDependencyChecker<V>
-    where
-        V: Hash + Eq + Clone,
+where
+    V: Hash + Eq + Clone,
 {
     // Set of elements that are already added.
     pool: HashSet<V>,
-    trace_back: HashMap<V, V>,
+    trace_back: HashMap<V, Vec<V>>,
 }
 
 impl<V> CyclicDependencyChecker<V>
-    where
-        V: Hash + Eq + Clone,
+where
+    V: Hash + Eq + Clone,
 {
     pub fn new() -> CyclicDependencyChecker<V> {
         CyclicDependencyChecker {
@@ -35,43 +35,80 @@ impl<V> CyclicDependencyChecker<V>
         }
     }
 
-    pub fn add(
-        &mut self,
-        element: V,
-        dependencies: &[V],
-    ) -> Result<(), CyclicDependencyError<V>> {
+    pub fn add(&mut self, element: V, dependencies: &[V]) -> Result<(), CyclicDependencyError<V>> {
         for dependency in dependencies {
+            for dependency in dependencies {
+                self.trace_back
+                    .entry(dependency.clone())
+                    .or_default()
+                    .push(element.clone());
+            }
             if self.pool.contains(dependency) && self.trace_back.contains_key(&element) {
                 return Err(CyclicDependencyError::new(
                     self.trace_back(&element, dependency),
                 ));
             }
         }
-        self.pool
-            .insert(element.clone());
-        for dependency in dependencies.into_iter() {
-            self.trace_back
-                .insert(dependency.clone(), element.clone());
-        }
+        self.pool.insert(element.clone());
         Ok(())
     }
 
     fn trace_back(&self, source: &V, target: &V) -> Vec<V> {
-        let mut vec = vec![source.clone()];
-        let mut cur = source;
-        loop {
-            let dependent = self
-                .trace_back
-                .get(cur)
-                .expect("Trace back unexpectedly failed since item to trace back is not present");
-            vec.push(dependent.clone());
-            if dependent == target {
-                break;
-            } else {
-                cur = dependent;
+        let mut work: HashSet<V> = HashSet::from_iter(self.trace_back.keys().cloned());
+        let mut distance = HashMap::<V, usize>::new();
+        let mut parents = HashMap::<V, V>::new();
+
+        for par in self.trace_back.get(source).unwrap() {
+            distance.insert(par.clone(), 1);
+        }
+
+        for v in self.trace_back.values().flatten() {
+            parents.insert(v.clone(), source.clone());
+        }
+        distance.insert(source.clone(), 0);
+        work.remove(source);
+
+        while !work.is_empty() {
+            let next = work
+                .iter()
+                .min_by(|a, b| {
+                    distance
+                        .get(a)
+                        .copied()
+                        .unwrap_or(usize::MAX)
+                        .cmp(&distance.get(b).copied().unwrap_or(usize::MAX))
+                })
+                .expect("No element but queue should not be empty")
+                .clone();
+            work.remove(&next);
+            for w in &work {
+                if distance.get(w).copied().unwrap_or(usize::MAX - 1)
+                    > distance.get(&next).copied().unwrap_or(usize::MAX - 1) + 1
+                {
+                    distance.insert(
+                        w.clone(),
+                        distance.get(&next).copied().unwrap_or(usize::MAX - 1) + 1,
+                    );
+                    parents.insert(w.clone(), next.clone());
+                }
             }
         }
-        vec
+
+        for element in &self.trace_back {
+            let (mut q, _) = element;
+            if q != target {
+                continue;
+            }
+
+            let mut path: Vec<V> = Vec::new();
+            path.push(q.clone());
+            while q != source {
+                q = parents.get(q).unwrap();
+                path.push(q.clone());
+            }
+            return path;
+        }
+        unreachable!("Should not happen")
     }
 }
 
@@ -128,7 +165,9 @@ mod tests {
         assert_eq!(checker.add(1, &[2]), Ok(()));
         assert_eq!(
             checker.add(2, &[1]),
-            Err(CyclicDependencyError { elements: vec![2, 1] })
+            Err(CyclicDependencyError {
+                elements: vec![1, 2]
+            })
         );
     }
 
@@ -141,8 +180,29 @@ mod tests {
         assert_eq!(
             checker.add(3, &[1]),
             Err(CyclicDependencyError {
-                elements: vec![3, 2, 1]
+                elements: vec![1, 2, 3]
             })
+        );
+    }
+
+    #[test]
+    fn cyclic_dependency_is_independent_of_order() {
+        let mut checker = CyclicDependencyChecker::new();
+
+        assert_eq!(checker.add(3, &[2]), Ok(()));
+        assert_eq!(checker.add(1, &[2]), Ok(()));
+        assert_eq!(
+            checker.add(2, &[3]),
+            Err(CyclicDependencyError::new(vec![3, 2]))
+        );
+
+        let mut checker = CyclicDependencyChecker::new();
+
+        assert_eq!(checker.add(1, &[2]), Ok(()));
+        assert_eq!(checker.add(3, &[2]), Ok(()));
+        assert_eq!(
+            checker.add(2, &[3]),
+            Err(CyclicDependencyError::new(vec![3, 2]))
         );
     }
 }
